44711ec2131bc76d1160e82e23ef27fd33b8ed35
thibsc <thibaut.bsc@gmail.com>
2022-05-18 23:28:30 -0400

    Fix reverse fahrenheit

--- src/utils/conversion.js
+++ src/utils/conversion.js
@@ -13,7 +13,10 @@ export const convert = (category, unitNameSrc, unitNameDest, value) => {
     // If the source unity is not the reference, convert it to simplify
     if (!srcIsReference){
         // If we have to convert, the * is / and + is -, and vice versa
-        value = formulaToValue(unitSrc.formula, value, true);
+        if (unitSrc.reverseFormula)
+            value = formulaToValue(unitSrc.reverseFormula, value, false);
+        else
+            value = formulaToValue(unitSrc.formula, value, true);
     }
     let newValue = formulaToValue(unitDest.formula, value, false);
     // Round to 2 decimals
