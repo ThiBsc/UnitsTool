1a4c88f1ae713303e28c84f7d621e5bbf92a6b93
Thibaut Biscay <tbiscay@uzinakod.com>
2022-06-18 14:43:18 -0400

    change the display of ListUnitItem

--- src/components/ListUnitItem.js
+++ src/components/ListUnitItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import TouchableScale from 'react-native-touchable-scale';
-import { Avatar, ListItem } from "@rneui/themed";
+import { ListItem } from "@rneui/themed";
 import { Button } from '@rneui/base';
 import { Text } from "react-native";
 import { useTranslation } from 'react-i18next';
@@ -16,24 +16,20 @@ const ListUnitItem = ({ unit, value, isReferenceUnit, setRefUnit }) => {
       containerStyle={{ borderRadius: 10, marginLeft: 10, marginRight: 10, marginTop: 5, marginBottom: 5 }}
       pad={20}
     >
-        <Avatar
-          size={32}
-          rounded
-          title={unit.symbol}
-          containerStyle={{ backgroundColor: 'dodgerblue' }}
-        />
-        <ListItem.Content>
-          <ListItem.Title>
-            <Text>{value}</Text>
-          </ListItem.Title>
-          <ListItem.Subtitle>
-            <Text>{t(unit.name)}</Text>
-          </ListItem.Subtitle>
-        </ListItem.Content>
+      <ListItem.Content>
+        <ListItem.Title>
+          <Text style={{fontWeight: 'bold'}}>{value}</Text>
+          {' '}
+          <Text>{unit.symbol}</Text>
+        </ListItem.Title>
+        <ListItem.Subtitle>
+          <Text style={{color: 'grey'}}>{t(unit.name)}</Text>
+        </ListItem.Subtitle>
+      </ListItem.Content>
 
-        <Button size='md' type='clear' color={'white'} disabled={isReferenceUnit} onPress={() => setRefUnit(unit)}>
-          <Icon name="star" solid={isReferenceUnit} size={24} color={isReferenceUnit ? 'orange' : 'gainsboro'}/>
-        </Button>
+      <Button size='md' type='clear' color={'white'} disabled={isReferenceUnit} onPress={() => setRefUnit(unit)}>
+        <Icon name="star" solid={isReferenceUnit} size={24} color={isReferenceUnit ? 'orange' : 'gainsboro'}/>
+      </Button>
     </ListItem>
   );
 }
